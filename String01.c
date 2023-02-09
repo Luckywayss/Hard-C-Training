eece1d75411c12c7f1072b18a71788675e7f2351
Janghyeon Han <wkdgus7385@gmail.com>
2023-02-09 16:32:12 +0900

    2-3.자료형,문자배열

---
+++ String01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch1 = 'A', ch2 = 'B', ch3 = 'C';//변수 선언 및 정의
+	char sData[4] = { 'A', 'B', 'C' };//문자 배열 선언 및 정의
+	char sNewData[4] = { "ABC" };//배열이란 개념은 같으나 연이어 묶는 방법의 차이
+	//문자 배열의 끝은 별다른 명시가 없으면 NULL문자('\0') 
+	return 0;
+}
