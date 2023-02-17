bf1394d33acdf3abd44d00344c41e73a01445c93
Janghyeon Han <wkdgus7385@gmail.com>
2023-02-17 11:23:06 +0900

    3-4 표준 I/O 도구, printf()

---
+++ floatformat01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double dData = 123.456;
+	printf("%f, %f\n", dData, -123.456);
+
+	//소수점 2번째 자리에서 반올림하고 1자리까지 출력
+	printf("%.1f\n", dData);
+	//소수점 3번째 자리에서 반올림하고 2자리까지 출력
+	printf("%.2f\n", dData);
+	//소수점 4번째 자리에서 반올림하고 3자리까지 출력
+	printf("%.3f\n", dData);
+	
+	//8자리로 출력
+	printf("%8d\n", 123);
+	//8자리로 출력하고 빈 공간은 0으로 출력
+	printf("%08d\n", 123);
+	//소수점을 포함해 12자리로 출력, 단 소수점 이하 4번째 자리에서
+	//반올림하고 3자리까지 출력한다.
+	printf("%12.3f\n", dData);
+	printf("%012.3f\n", dData);
+
+	return 0;
+}
